7c68eeca44db01509a87b79d1846e2b47fcee6e9
Judith Pillado <judith.pillado@gmail.com>
2020-12-05 20:20:43 -0600

    Replace if conditional with 'switch' and case branches

--- app/staticCalculator/Calculator.java
+++ app/staticCalculator/Calculator.java
@@ -10,18 +10,27 @@ public class Calculator {
     // int can only hold integers.
     // int values are not objects
     // primitive datatypes can only hold data whereas the classes also provide methods to manipulate the data the variable holds
-    if (operation == 'A') {
-			result = a + b;
-		} else if (operation == 'S') {
-			result = a - b;
-		} else if (operation == 'M') {
-			result = a * b;
-		} else if (operation == 'D') {
-			result = a / b;
-		} else {
-			System.out.println("Only the operations 'A', 'S', 'M' and 'D' are allowed.");
-			return;
-      // Problematic - it tells the program to quit immediately
+    switch (operation) {
+      // you can use 'switch' if you don't want to use if statements
+			case 'A':
+      // 'case' replaces 'if'
+      // 'case branch' is the proper way to call it?
+				result = a + b;
+				break;
+        // 'break' statement is important bc otherwise it would execute multiple cases.
+          // eg: case 'M' and default branch (which is the 'else' statement) might execute 
+			case 'S':
+				result = a - b;
+				break;
+			case 'M':
+				result = a * b;
+				break;
+			case 'D':
+				result = a / b;
+				break;
+			default:
+				System.out.println("Only the operations 'A', 'S', 'M' and 'D' are allowed.");
+				return;
 		}
 		System.out.println("Result: " + result);
     // System.out.println is a METHOD that is used to print the result in the console
