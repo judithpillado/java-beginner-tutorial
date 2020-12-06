80c29364549c4a2ec3d4e93a9067c72dd4de6a63
Judith Pillado <judith.pillado@gmail.com>
2020-12-05 19:28:28 -0600

    Finish first program that calculates value in Calculator.java

---
+++ app/staticCalculator/Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+	public static void main(String[] args) {
+    int a = 1;
+		int b = 2;
+    int result = a + b;
+    // a, b, and result are all variables
+		System.out.println("Result: " + result);
+    // System.out.println is a METHOD that is used to print the result in the console
+    // 1. $ javac Calculator.java
+      // this creates the Calculator.class
+    // 2. $ java -cp . Calculator
+      // Result: 3 
+	}
+}
